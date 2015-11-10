51fedaa596e68f3650f457a8475e56908d2c084e
twinaphex <libretro@gmail.com>
2015-11-10 02:59:52 +0100

    Update

--- file/file_path.c
+++ file/file_path.c
@@ -364,7 +364,8 @@ void fill_pathname_base(char *out, const char *in_path, size_t size)
 void fill_pathname_basedir(char *out_dir,
       const char *in_path, size_t size)
 {
-   retro_assert(strlcpy(out_dir, in_path, size) < size);
+   if (out_dir != in_path)
+      retro_assert(strlcpy(out_dir, in_path, size) < size);
    path_basedir(out_dir);
 }
 
@@ -380,7 +381,8 @@ void fill_pathname_basedir(char *out_dir,
 void fill_pathname_parent_dir(char *out_dir,
       const char *in_dir, size_t size)
 {
-   retro_assert(strlcpy(out_dir, in_dir, size) < size);
+   if (out_dir != in_dir)
+      retro_assert(strlcpy(out_dir, in_dir, size) < size);
    path_parent_dir(out_dir);
 }
 
