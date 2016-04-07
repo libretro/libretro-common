5b6a2d9063d6ea1f566c505a0c6b28d7f7738117
twinaphex <libretro@gmail.com>
2016-04-07 04:20:36 +0200

    Cleanups

--- streams/file_stream.c
+++ streams/file_stream.c
@@ -240,27 +240,23 @@ error:
 
 ssize_t filestream_seek(RFILE *stream, ssize_t offset, int whence)
 {
-   int ret = 0;
    if (!stream)
       goto error;
 
-   (void)ret;
-
 #if defined(VITA) || defined(PSP)
-   ret = sceIoLseek(stream->fd, (SceOff)offset, whence);
-   if (ret == -1)
+   if (sceIoLseek(stream->fd, (SceOff)offset, whence) == -1)
       goto error;
-   return 0;
 #elif defined(__CELLOS_LV2__)
    uint64_t pos = 0;
    if (cellFsLseek(stream->fd, offset, whence, &pos) != CELL_FS_SUCCEEDED)
       goto error;
-   return 0;
 #else
+
 #if defined(HAVE_BUFFERED_IO)
    if ((stream->hints & RFILE_HINT_UNBUFFERED) == 0)
       return fseek(stream->fp, (long)offset, whence);
 #endif
+
 #ifdef HAVE_MMAP
    /* Need to check stream->mapped because this function is 
     * called in filestream_open() */
@@ -295,11 +291,13 @@ ssize_t filestream_seek(RFILE *stream, ssize_t offset, int whence)
       return stream->mappos;
    }
 #endif
-   ret = lseek(stream->fd, offset, whence);
-   if (ret < 0)
+
+   if (lseek(stream->fd, offset, whence) < 0)
       goto error;
-   return ret;
+
 #endif
+
+   return 0;
 
 error:
    return -1;
@@ -310,12 +308,12 @@ ssize_t filestream_tell(RFILE *stream)
    if (!stream)
       goto error;
 #if defined(VITA) || defined(PSP)
-   return sceIoLseek(stream->fd, 0, SEEK_CUR);
+  if (sceIoLseek(stream->fd, 0, SEEK_CUR) < 0)
+     goto error;
 #elif defined(__CELLOS_LV2__)
    uint64_t pos = 0;
    if (cellFsLseek(stream->fd, 0, CELL_FS_SEEK_CUR, &pos) != CELL_FS_SUCCEEDED)
       goto error;
-   return 0;
 #else
 #if defined(HAVE_BUFFERED_IO)
    if ((stream->hints & RFILE_HINT_UNBUFFERED) == 0)
@@ -327,8 +325,11 @@ ssize_t filestream_tell(RFILE *stream)
    if (stream->mapped && stream->hints & RFILE_HINT_MMAP)
       return stream->mappos;
 #endif
-   return lseek(stream->fd, 0, SEEK_CUR);
+   if (lseek(stream->fd, 0, SEEK_CUR) < 0)
+      goto error;
 #endif
+
+   return 0;
 
 error:
    return -1;
