82469c24b7370691efdf0dafd18e7c91e33911c7
twinaphex <libretro@gmail.com>
2016-05-04 02:10:27 +0200

    Update

--- include/net/net_compat.h
+++ include/net/net_compat.h
@@ -211,22 +211,12 @@ struct addrinfo
 
 #endif
 
+uint16_t inet_htons(uint16_t hostshort);
+
 int getaddrinfo_retro(const char *node, const char *service,
-      const struct addrinfo *hints,
-      struct addrinfo **res);
+      struct addrinfo *hints, struct addrinfo **res);
 
 void freeaddrinfo_retro(struct addrinfo *res);
-
-bool socket_nonblock(int fd);
-
-int socket_close(int fd);
-
-int socket_select(int nfds, fd_set *readfs, fd_set *writefds,
-      fd_set *errorfds, struct timeval *timeout);
-
-int socket_send_all_blocking(int fd, const void *data_, size_t size);
-
-int socket_receive_all_blocking(int fd, void *data_, size_t size);
 
 /**
  * network_init:
