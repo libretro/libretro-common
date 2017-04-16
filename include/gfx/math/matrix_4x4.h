c44d03a0f6b3c9b5b929775e907962b8a77f4a87
twinaphex <libretro@gmail.com>
2017-04-16 04:57:12 +0200

    Update

--- include/gfx/math/matrix_4x4.h
+++ include/gfx/math/matrix_4x4.h
@@ -81,7 +81,6 @@ typedef struct math_matrix_4x4
    MAT_ELEM_4X4(mat, 3, 2)    = 0.0f; \
    MAT_ELEM_4X4(mat, 3, 3)    = 1.0f
 
-void matrix_4x4_copy(math_matrix_4x4 *dst, const math_matrix_4x4 *src);
 void matrix_4x4_transpose(math_matrix_4x4 *out, const math_matrix_4x4 *in);
 
 /*
