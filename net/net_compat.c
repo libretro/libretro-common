d2fc28cf58581fea18bc74baadcd14f0fd5cff21
twinaphex <libretro@gmail.com>
2017-05-17 07:33:54 +0200

    Updates

--- net/net_compat.c
+++ net/net_compat.c
@@ -354,3 +354,36 @@ int inet_ptrton(int af, const char *src, void *dst)
    return inet_pton(af, src, dst);
 #endif
 }
+
+struct in_addr6_compat
+{
+   unsigned char ip_addr[16];
+};
+
+const char *inet_ntop_compat(int af, const void *src, char *dst, socklen_t cnt)
+{
+   if (af == AF_INET)
+   {
+      struct sockaddr_in in;
+      memset(&in, 0, sizeof(in));
+      in.sin_family = AF_INET;
+      memcpy(&in.sin_addr, src, sizeof(struct in_addr));
+      getnameinfo((struct sockaddr *)&in, sizeof(struct
+               sockaddr_in), dst, cnt, NULL, 0, NI_NUMERICHOST);
+      return dst;
+   }
+#if defined(AF_INET6) && !defined(HAVE_SOCKET_LEGACY)
+   else if (af == AF_INET6)
+   {
+      struct sockaddr_in6 in;
+      memset(&in, 0, sizeof(in));
+      in.sin6_family = AF_INET6;
+      memcpy(&in.sin6_addr, src, sizeof(struct in_addr6_compat));
+      getnameinfo((struct sockaddr *)&in, sizeof(struct
+               sockaddr_in6), dst, cnt, NULL, 0, NI_NUMERICHOST);
+      return dst;
+   }
+#endif
+
+   return NULL;
+}
