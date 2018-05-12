34bfa46fdfad25806d9ffdef2f76ddf1fae6ec49
markwkidd <mark@stardart.net>
2018-05-12 11:23:27 -0500

    keep char slash macro

--- include/file/file_path.h
+++ include/file/file_path.h
@@ -454,7 +454,7 @@ void path_basedir_wrapper(char *path);
 #endif
 
 /**
- * path_default_slash:
+ * path_default_slash and path_default_slash_c:
  *
  * Gets the default slash separator.
  *
@@ -462,8 +462,10 @@ void path_basedir_wrapper(char *path);
  */
 #ifdef _WIN32
 #define path_default_slash() "\\"
+#define path_default_slash_c() '\\'
 #else
 #define path_default_slash() "/"
+#define path_default_slash_c() '/'
 #endif
 
 /**
