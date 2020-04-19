9dafa73948c7f9f1fc87706a6603403586e32d7a
Vladimir Serbinenko <phcoder@google.com>
2020-04-19 20:30:07 +0200

    Spelling fix

--- include/retro_endianness.h
+++ include/retro_endianness.h
@@ -385,7 +385,7 @@ static INLINE uint32_t load32be(const uint32_t *addr)
 #define retro_be_to_cpu64(val) swap_if_little64(val)
 
 #ifdef  __GNUC__
-/* This attribute means that the same memory may be refferred through
+/* This attribute means that the same memory may be referred through
    pointers to different size of the object (aliasing). E.g. that u8 *
    and u32 * may actually be pointing to the same object.  */
 #define MAY_ALIAS  __attribute__((__may_alias__))
