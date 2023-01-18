9c644e2386f0398b1b7e0509a731453b9abf6155
Giovanni Cascione <ing.cascione@gmail.com>
2023-01-18 20:44:16 +0100

    rthreads: include sys/time.h for mips and 3ds this is to cover all platforms where struct timeval is used

--- rthreads/rthreads.c
+++ rthreads/rthreads.c
@@ -55,7 +55,7 @@
 #include <time.h>
 #endif
 
-#if defined(VITA) || defined(BSD) || defined(ORBIS)
+#if defined(VITA) || defined(BSD) || defined(ORBIS) || defined(__mips__) || defined(_3DS)
 #include <sys/time.h>
 #endif
 
